5a0b93a4b2d03f1bb7e1b145347820d1a1391495
Sandeep Dahiya <sandeep.dahiya@gigamon.com>
2024-12-28 16:57:20 +0530

    Linted python code

--- fiddle.vs/my-app/Python/fibonacci.py
+++ fiddle.vs/my-app/Python/fibonacci.py
@@ -1,9 +1,9 @@
 def fibonnaci(n):
     if n <= 1:
         return 1
-    else:
-        return fibonnaci(n-1) + fibonnaci(n-2)
-    
+
+    return fibonnaci(n-1) + fibonnaci(n-2)
+
 print(fibonnaci(0))
 print(fibonnaci(1))
 print(fibonnaci(2))
